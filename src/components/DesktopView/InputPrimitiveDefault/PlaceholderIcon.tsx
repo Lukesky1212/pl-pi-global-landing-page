783d6eb01577072698baa49b02363e4fe75f5043
Lucas Atala <lucas.atala@ppro.com>
2024-02-26 16:04:11 -0300

    Added repository

--- src/components/DesktopView/InputPrimitiveDefault/PlaceholderIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const PlaceholderIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <circle cx={12} cy={12} r={12} fill='#EB00FF' />
-  </svg>
-);
-const Memo = memo(PlaceholderIcon);
-export { Memo as PlaceholderIcon };
